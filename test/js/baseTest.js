e8a79860dd0b9adf5d6f3a51f654a584bb9e0713
chenguangxi <brave_cgx@163.com>
2016-03-03 14:55:16 +0800

    1.增加了对core文件中3个文件的测试和适用例子。

--- test/js/baseTest.js
+++ test/js/baseTest.js
@@ -14,24 +14,52 @@ test.Box = (function(window,$,gn){
 
     gn.Core.inherits(gn.GNObject,Box);
 
-    Box.prototype.initialize = function(){
-        createChildren();
-        childrenCreated();
-    };
-
-    function createChildren(){
-
-    }
-
-    function childrenCreated(){
-
-    }
-
     return Box;
 
 })(window,jQuery,gardener);
 
-(function(window,$,gn){
+(function(window,$,gn,test){
     "use strict";
 
-})(window,jQuery,gardener);
+    var boxList,sum=10;
+
+    $(document).ready(initialize);
+
+    /**
+     * init
+     */
+    function initialize(){
+        boxList = [];
+        var box;
+
+        while(sum--){
+            box = new test.Box();
+            boxList.push(box);
+            console.log("gnId:",box.gnId,"output:",box.output());
+        }
+        console.log("****************");
+        box = gn.OM.getGNObject(boxList[1].gnId);
+        console.log("Get a box in gn.OM==>",box,box.gnId,box.className);
+        console.log("Change a gnId before:",boxList[2].gnId);
+        gn.OM.changeGNId("box02",boxList[2].gnId);
+        console.log("Change a gnId after:",boxList[2].gnId);
+        console.log("Does a box in gn.OM?==>",gn.OM.inGNList(boxList[3].gnId));
+        console.log("How many does gn.OM have?===>",gn.OM.length());
+        console.log("Let's destroy all boxes.");
+        for(var i= 0;(box=boxList[i])!=null;i++){
+            box.terminalClear(false); //The param of 'false' is optional.
+        }
+        console.log('Look,gn.OM has any GNObject in it.===>',gn.OM.length());
+        /*==== Let's test the create a instance by a gnObject's className =====*/
+        //we can do this:
+        var ClassItem = gn.Core.getDefinitionByName('test.Box');
+        box = new ClassItem();
+        console.log(box.gnId,gn.OM.length());
+
+        box.terminalClear();
+        //we also can use this:
+        box = gn.Core.createInstance('test.Box');
+        console.log(box.gnId,gn.OM.length());
+    }
+
+})(window,jQuery,gardener,test);
